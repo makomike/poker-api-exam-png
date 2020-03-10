71c20162e419f6e68435a524690708b019694860
utallion <johnmichael.hilario@gmail.com>
2020-03-11 03:29:24 +0800

    First commit: Creating Deck of Cards.

---
+++ pokerAPI/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pokerAPI
+{
+
+    public class Deck
+    {
+
+        int[] rank = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        string[] suit = { "D", "H", "S", "C" };
+
+        private List<string> cardDeck;
+
+        public Deck() {
+            cardDeck = new List<string>();
+           
+        }
+
+        public List<string> currentCardDeck { get { return cardDeck; } }
+        
+        public void CreateDeck() //Creating of deck
+        {
+         
+            foreach (String s in suit) {
+                foreach (int i in rank) {
+                    cardDeck.Add(i+s);
+                }
+            }
+
+        }
+
+        
+
+    }
+}
